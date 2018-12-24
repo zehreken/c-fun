6308866c122c278c47328d1cdda756c6c03c9d99
zehreken <zehreken@gmail.com>
2018-12-24 22:09:45 +0300

    Add some fun stuff

--- source/main.c
+++ source/main.c
@@ -2,6 +2,13 @@
 
 int main()
 {
-    printf("test\n");
+    int n = 016; // Octal values start with 0
+    printf("%d\n", n);
+    
+    int x = 0x16; // Hex values start with 0x
+    printf("%d\n", x);
+    
+    int b = 0b1111;
+    printf("%d\n", b); // Binary values start with 0b
     return 0;
 }
