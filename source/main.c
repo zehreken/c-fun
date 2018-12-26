c47b62ec6f0d7610030b3f562131bc8bf66102fa
zehreken <zehreken@gmail.com>
2018-12-26 21:47:01 +0300

    Add printAsBinary

--- source/main.c
+++ source/main.c
@@ -1,14 +1,37 @@
 #include "stdio.h"
+#include "string.h"
+
+void printAsBinary(int);
 
 int main()
 {
     int n = 016; // Octal values start with 0
-    printf("%d\n", n);
+    printAsBinary(n);
     
     int x = 0x16; // Hex values start with 0x
     printf("%d\n", x);
     
     int b = 0b1111;
     printf("%d\n", b); // Binary values start with 0b
+    
+    int a = 100;
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%u\n", a);
+        a = a >> 1;
+    }
     return 0;
+}
+
+void printAsBinary(int n)
+{
+    char b[9];
+    b[0] = '\0';
+    
+    for (int z = 128; z > 0; z >>= 1)
+    {
+        strcat(b, ((n & z) == z) ? "1" : "0");
+    }
+    
+    printf("%s\n", b);
 }
