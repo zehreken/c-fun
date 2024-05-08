f6885f35d7b4b239c3a05459860e0e5bc9927c08
zehreken <zehreken@gmail.com>
2024-05-08 23:46:35 +0200

    Add binary to float calculation

--- source/float_fun.c
+++ source/float_fun.c
@@ -3,5 +3,10 @@
 
 void floatFunInit()
 {
-    printf("test");
+    unsigned int binaryRep = 0b11110001100000111000000000000000;
+    float f1;
+    *((unsigned int *)&f1) = binaryRep;
+
+    printf("bR: %d\n", binaryRep);
+    printf("f1: %f\n", f1);
 }
